f395ce666780264a61f137ad92244e168a094ea5
Igor Rak <igorrakfed@gmail.com>
2021-11-25 15:16:30 +0100

    brauser check resegnation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,3 @@
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 import Link from "next/link";
 import Image from "next/image";
 import { FC } from "react";
@@ -8,34 +7,21 @@ import styles from "../styles/Header.module.scss";
 const Header:FC=() => {
     return (  
     <header>
-        <BrowserView>
-        <div className={styles.container}>
+     
+        <div className={styles.container}>                  
+
+            <Link href="/">
+                <a>
+                    <Image src="/logo.png" width={60} height={60} alt="webDev" />
+                </a>
+            </Link>
             
-            <div className={styles.logo}> 
-            <Link href={`/`}>         
-                  <Image src="/logo.png" width={60} height={60} alt="webDev" />
-             </Link>    
-             
-            </div>
-           
-    
-                 <Navbar/> 
-            </div>
-        </BrowserView>
-        <MobileView>
-        <div className={styles.container}>
-            
-            <div className={styles.logo}> 
-            <Link href={`/`}>         
-                  <Image src="/logo.png" width={30} height={30} alt="webDev" />
-             </Link>    
-             
-            </div>
-           
-    
-                 <Navbar/> 
-            </div>
-        </MobileView>
+        
+            <Navbar/> 
+
+        </div>
+       
+        
        
             
     </header>);
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,57 +1,31 @@
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import styles from "../styles/Navbar.module.scss";
-import { useState } from 'react';
+// import { useState } from 'react';
 
 
 const navigation = [
     { id: 1, title: 'Home', path: '/' },
     { id: 2, title: 'SSG', path: '/posts' },
      { id: 3, title: 'SSR', path: '/contacts' },
-     { id: 4, title: 'Images', path: '/images' },
+    //  { id: 4, title: 'Images', path: '/images' },
   ];
 
 const Navbar = () => {
     const { pathname } = useRouter();
     
-    const [menu,setMenu] = useState(false)
+    // const [menu,setMenu] = useState(false)
 
   
     return (
       <nav className={styles.nav}>
-         <BrowserView>
          <div className={styles.links}>
-          {navigation.map(({ id, title, path }) => (
-            <Link key={id} href={path}>
-              <a className={pathname === path ? styles.active : null}>{title}</a>
-            </Link>
-          ))}
-          <Link  href={'/images'}>
-              <a className={pathname === '/images' ? styles.active : null}>Images</a>
-          </Link>
-            
+            {navigation.map(({ id, title, path }) => (
+              <Link key={id} href={path}>
+                <a className={pathname === path ? styles.active : null}>{title}</a>
+              </Link>
+            ))}        
         </div>
-        </BrowserView>
-        <MobileView>
-            <div className={styles.menu__icon} onClick={()=>{setMenu(!menu)}}><span></span></div>
-            {menu&&<div className="menu"> 
-             {navigation.map(({id, title, path})=>{ return (<h1 className={styles.item}
-              onClick={()=>{setMenu(!menu)}}>              
-               <Link  key={id }href={path}>
-               <a>{title}</a>
-           </Link>
-           </h1>)
-               
-             })}
-            
-            </div>}
-
-            {/* <div className="menu__icon _active"><span></span></div> */}
-        </MobileView>
-        
-        
       </nav>
     );
   };
